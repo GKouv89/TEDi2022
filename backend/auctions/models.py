805084bc0fd5e8785d9da74426804697ec5764ae
GKouv89 <gina101@windowslive.com>
2022-08-26 03:22:38 -0700

    Bids model probs okay?

--- backend/auctions/models.py
+++ backend/auctions/models.py
@@ -3,14 +3,20 @@ from base.models import MyUser, Address
 
 # Create your models here.
 class Item(models.Model):
-    # Is there an id field created?
     name = models.CharField(max_length=255)
     currently =  models.DecimalField(default=0.0, max_digits=6, decimal_places=2)
     first_bid = models.DecimalField(default=0.0, max_digits=6, decimal_places=2)
     buy_price = models.DecimalField(null=True, blank=True, max_digits=6, decimal_places=2)
     number_of_bids = models.IntegerField(default=0)
     # Category field missing
-    # Relationship with bids model missing so far
+
+    bids = models.ManyToManyField(
+        'base.MyUser',
+        through='Bid',
+        through_fields=('item', 'bidder'),
+        related_name='bidders',
+    )
+
     address = models.ForeignKey(
         'base.Address',
         on_delete = models.CASCADE,
@@ -18,7 +24,21 @@ class Item(models.Model):
     seller = models.ForeignKey(
         'base.MyUser',
         on_delete=models.CASCADE,
+        related_name='seller',
     )
     started = models.DateTimeField()
     ended = models.DateTimeField()
     description = models.TextField()
+
+class Bid(models.Model):
+    bidder = models.ForeignKey(
+        'base.MyUser',
+        on_delete=models.CASCADE,
+        related_name='bidder',
+    )
+    item = models.ForeignKey(
+        'Item',
+        on_delete=models.CASCADE,
+    )
+    time = models.DateTimeField()
+    amount = models.DecimalField(max_digits=6, decimal_places=2)
