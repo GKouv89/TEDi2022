4ed12fbac6f99a170b1998a4259ff45629067911
GKouv89 <gina101@windowslive.com>
2022-09-10 07:57:08 -0700

    Some aesthetic tweaks

--- frontend/src/pages/Auctions/AuctionSearch.js
+++ frontend/src/pages/Auctions/AuctionSearch.js
@@ -4,6 +4,7 @@ import axios from 'axios'
 import ItemCard, { SkeletonCard } from "../../components/Auctions/Browsing/ItemCard"
 import MyPagination from '../../components/MyPagination'
 import Grid from '@mui/material/Grid'
+import Stack from '@mui/material/Stack'
 
 function AuctionSearch(){
     const pageSize = 12; // Fixed number, same as backend
@@ -34,27 +35,33 @@ function AuctionSearch(){
 
     return(
         <>
-            <Grid container spacing={2}>
-                {
-                    loaded ?
-                    data.map((item, idx) => <Grid item xs={4}><ItemCard data={item} key={idx} loaded={loaded}/></Grid>)
-                    :
-                    <>
-                        <Grid item xs={4}>
-                            <SkeletonCard />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <SkeletonCard />
-                        </Grid>
-                        <Grid item xs={4}>
-                            <SkeletonCard />
-                        </Grid>
-                    </>
-                }
-                <Grid item xs={12}>
-                    <MyPagination count={count} callback={paginationCallback}/>
+            <Stack spacing={2}>
+                <Grid container spacing={2} justifyContent="center">
+                    {
+                        loaded ?
+                        data.map((item, idx) => <Grid item xs={4}><ItemCard data={item} key={idx} loaded={loaded}/></Grid>)
+                        :
+                        <>
+                            <Grid item xs={4}>
+                                <SkeletonCard />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <SkeletonCard />
+                            </Grid>
+                            <Grid item xs={4}>
+                                <SkeletonCard />
+                            </Grid>
+                        </>
+                    }
                 </Grid>
-            </Grid>
+                <Grid container justifyContent="center">
+                    <Grid item xs={4}></Grid>
+                    <Grid item xs={2}>
+                        <MyPagination count={count} callback={paginationCallback}/>
+                    </Grid>
+                    <Grid item xs={4}></Grid>
+                </Grid>
+            </Stack>
         </>
     )
 }
